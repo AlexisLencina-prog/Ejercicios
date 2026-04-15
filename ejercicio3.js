bb722e74a378f5fc4da6f1112c9e468accfcbc56
AlexisLencina-prog <alexisml875@gmail.com>
2026-04-15 00:04:56 -0300

    ejercicio 3 al 6

---
+++ ejercicio3.js
@@ -0,0 +1,12 @@
+let usuario="admin"
+let contraseña="1234"
+
+if (usuario=="admin" ) {
+    if (contraseña=="1234") {
+        console.log("Acceso concedido")
+    } else {
+        console.log("Acceso Denegado")
+    }
+    } else {
+        console.log("Acceso Denegado")
+    }
